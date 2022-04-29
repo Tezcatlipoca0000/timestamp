1976d43e6c2ccf0b6345ac60f1fc0e0280f658cb
Tezcatlipoca <tezcatlipoca00001@gmail.com>
2022-04-29 10:53:06 -0500

    created an enviroment file to store the port number, updated the package json and lock json to my git url and solved the fcc challenge on server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -14,19 +15,103 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', (req, res) =>{
+  let inputDate = req.params.date,
+    gmt = new Date(inputDate).toUTCString(),
+    unx = Math.floor(new Date(Number(inputDate)).getTime()),
+    inv = {error: 'Invalid Date'},
+    result = {unix: unx, utc: gmt};
+  if (inputDate === '' || inputDate === undefined) {
+    inputDate = new Date();
+    gmt = inputDate.toUTCString();
+    unx = inputDate.getTime();
+    result.unix = unx;
+    result.utc = gmt;
+    res.json(result);
+  } else if (gmt == 'Invalid Date' && Number.isNaN(unx)) {
+    res.json(inv);
+  } else if (gmt == 'Invalid Date' && Number(unx)) {
+    gmt = new Date(Number(inputDate)).toUTCString();
+    result.utc = gmt;
+    res.json(result);
+  } else if (gmt != 'Invalid Date' && Number.isNaN(unx)) {
+    unx = Math.floor(new Date(inputDate).getTime());
+    result.unix = unx;
+    res.json(result);
+  }
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+let listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+/* v1
+
+app.get('/api/:date', (req, res) =>{
+  let date = req.params.date,
+    err = {error: 'Invalid Date'},
+    gmt,
+    unx,
+    re = /-/,
+    x = new Date(date);
+  console.log(x);
+  if (!date) {
+    console.log('empty');
+  }
+  if (re.test(date)) {
+    if (new Date(date) == 'Invalid Date') {
+      return res.json(err);
+    }
+    gmt = new Date(date).toUTCString();
+    unx = Math.floor(new Date(date).getTime());
+  } else {
+    if (new Date(Number(date)).toUTCString() == 'Invalid Date') {
+      return res.json(err);
+    }
+    gmt = new Date(Number(date)).toUTCString();
+    unx = Number(date);
+  }
+  res.json({unix: unx, utc: gmt});
+});
+
+*/
+
+/* v2
+
+app.get('/api/:date', (err, req, res) =>{
+  let date = new Date(req.params.date),
+    err = {error: 'Invalid Date'},
+    gmt,
+    unx,
+    re = /-/,
+  console.log(date);
+  if (!date) {
+    console.log('empty');
+  }
+  if (date == 'Invalid Date') {
+    return res.json(err);
+  }
+  if (re.test(req.params.date)) {
+    gmt = date.toUTCString();
+    unx = Math.floor(date.getTime());
+  } else {
+    gmt = date.toUTCString();
+    unx = Math.floor(date.getTime());
+  }
+  res.json({unix: unx, utc: gmt});
+});
+
+*/
